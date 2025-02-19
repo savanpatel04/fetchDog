f29513a437e681b3e92b993368aa3b311136ef1d
Savan Desai <savandesai@Savans-MacBook-Pro.local>
2025-02-18 23:47:03 -0600

    change in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import DogSearch from './components/DogSearch';
 import Login from './components/Login';
 
