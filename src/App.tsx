d239155845571bf15a7e9763ac4309a66acb6d7e
Savan Desai <savandesai@Savans-MacBook-Pro.local>
2025-02-18 23:39:09 -0600

    add new file

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Login from './components/Login';
 
 function App() {
   return (
-    <Router basename="/fetchDog">
+    <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/search" element={<DogSearch />} />
